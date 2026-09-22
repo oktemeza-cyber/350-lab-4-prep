410f26d9011120667a368e4771116844aac76e81
oktemeza-cyber <oktemeza@ualberta.ca>
2026-09-21 23:46:22 -0600

    Implemented timer and expand--made sure expand works

--- src/expand.cpp
+++ src/expand.cpp
@@ -5,10 +5,17 @@
 // Expands the binary representation of input by a factor of scale.
 // e.g., expand(0b1111ull, 3) == 0b001001001001
 uint64_t expand(uint64_t input, uint32_t scale) {
-    return 0;  // replace this with your impl!
+    assert(scale < 64);
+    uint64_t result = 0;
+    for(uint32_t i = 0; i < 64; i++){
+        uint64_t bit = (input >> i) & 1ull;
+        result |= bit << (i * scale);
+    }
+    return result;  // replace this with your impl!
 }
 
 int main() {
-    // test here...
+    assert(expand(0b1111ull, 3) == 0b001001001001);
+    assert(expand(0b0101,2) == 0b00010001);
     return 0;
 }
--- src/timer.h
+++ src/timer.h
@@ -1,4 +1,47 @@
 #ifndef TIMER_H
 #define TIMER_H
 
+#include <chrono>
+#include <cstdint>
+
+class Timer {
+    public:
+        using Nanos = std::chrono::nanoseconds;
+        using Micros = std::chrono::microseconds;
+        using Millis = std::chrono::milliseconds;
+        using Seconds = std::chrono::seconds;
+        using Minutes = std::chrono::minutes;
+        using Hours = std::chrono::hours;
+
+        Timer(){
+            startTime = currentTime = lastTime = std::chrono::steady_clock::now();
+        }
+        void Restart(){
+            startTime = std::chrono::steady_clock::now();
+        }
+
+        template <typename T>
+        uint64_t click(){
+            currentTime = std::chrono::steady_clock::now();
+            uint64_t elapsedTime = std::chrono::duration_cast<T>(currentTime - lastTime).count();
+            lastTime = currentTime;
+            return elapsedTime;
+        }
+
+        template <typename T>
+        uint64_t glance() {
+            currentTime = std::chrono::steady_clock::now();
+            uint64_t elapsedTime = std::chrono::duration_cast<T>(currentTime - lastTime).count();
+            return elapsedTime;
+        }
+    
+    private:
+        std::chrono::steady_clock::time_point startTime;
+        std::chrono::steady_clock::time_point currentTime;
+        std::chrono::steady_clock::time_point lastTime;
+
+
+
+};
+
 #endif  // TIMER_H
